f157b7faa11701b3687c4c07ba4165096bbccf81
Michael De Checchi <mdechecchi@jahia.com>
2025-01-17 17:43:36 +0100

    Update commit message

--- src/github.ts
+++ src/github.ts
@@ -323,10 +323,13 @@ const createGitHubRepository = TE.tryCatchK<Error, [CreateGitHubRepositoryParams
           passphrase
         });
 
+        const now = Date.now();
         const commitMessage = await openpgp.createMessage({
           text: [
             'tree ' + tree,
             'parent ' + parent,
+            'author Jahia CI <jahia-ci@jahia.com> ' + Math.floor(now / 1000) + ' +0000',
+            'committer Jahia CI <jahia-ci@jahia.com> ' + Math.floor(now / 1000) + ' +0000',
             '',
             message,
           ].join('\n')
